6ab86b7bf276d7a3a0fc69edbf10a0267f187cb2
mejison <mejison@ex.ua>
2016-12-11 18:03:33 +0200

    add dump DB

---
+++ dump.sql
@@ -0,0 +1,39 @@
+/*
+SQLyog Ultimate v12.14 (64 bit)
+MySQL - 5.1.73-community-log : Database - test
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*Table structure for table `car_items` */
+
+DROP TABLE IF EXISTS `car_items`;
+
+CREATE TABLE `car_items` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `price` int(11) DEFAULT NULL,
+  `desc` text,
+  `old_price` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+
+/*Data for the table `car_items` */
+
+insert  into `car_items`(`id`,`name`,`price`,`desc`,`old_price`) values 
+(1,' Blue Car',234,' text\r\n',120),
+(2,' Red Car',234,' text\r\n',130),
+(3,' Yellow Car',234,' text\r\n',120),
+(4,' Black Car',234,' text',110);
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
